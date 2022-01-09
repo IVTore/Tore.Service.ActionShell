c2fb4bdf39b3a26877927ffa282c32011874fb63
Volkan Töre <volkan@fenomen.org>
2022-01-09 12:53:49 +0300

    Add project files.

---
+++ ActionShell.cs
@@ -0,0 +1,118 @@
+﻿/*————————————————————————————————————————————————————————————————————————————
+    ——————————————————————————————————————————————
+    |   ActionShell : Endpoint entry exit.       |
+    ——————————————————————————————————————————————
+
+    © Copyright 2020 İhsan Volkan Töre.
+
+Author              : IVT.  (İhsan Volkan Töre)
+Version             : 202201070900
+License             : MIT.
+
+History             :
+202201070900: IVT   : added.
+————————————————————————————————————————————————————————————————————————————*/
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using Microsoft.AspNetCore.Http;
+
+namespace Tore.Service {
+
+    /**———————————————————————————————————————————————————————————————————————————
+        CLASS:  ActionShell.                                            <summary>
+        USAGE:  Builds a request scoped context data.
+                enter delegate is called before entry to endpoint.
+                leave delegate is called after  endpoint is run.
+                The request scope is passed to these delegates and since
+                it is also stored into HttpContext, it is accessible from the 
+                controller and even Global exception middleware in case of 
+                any exceptions.                                         </summary>
+    ————————————————————————————————————————————————————————————————————————————*/
+    public class ActionShell : ActionFilterAttribute {
+
+        private static Type _requestScopeType = typeof(RequestScopeBase);
+
+        /**———————————————————————————————————————————————————————————————————————————
+          TYPE:  OnEnterDelegate                                            <summary>
+          TASK:  Method delegate type to call before entry to endpoint.     <br/>
+          ARGS:  scope: RequestScopeBase : Current request scope instance.  </summary>
+        ————————————————————————————————————————————————————————————————————————————*/
+        public delegate void OnEnterDelegate(RequestScopeBase scope);
+        /**———————————————————————————————————————————————————————————————————————————
+          TYPE:  OnLeaveDelegate                                            <summary>
+          TASK:  Method delegate type to call after leaving the endpoint.   <br/>
+          ARGS:  scope: RequestScopeBase : Current request scope instance.  </summary>
+        ————————————————————————————————————————————————————————————————————————————*/
+        public delegate void OnLeaveDelegate(RequestScopeBase scope);
+
+        /**———————————————————————————————————————————————————————————————————————————
+          PROP: enter.                                                  <summary>
+          TASK: Method delegate to call before entry to endpoint.       <br/>
+                Method delegate type must be OnEnterDelegate.           <br/>
+                The method should be static.                            </summary>
+        ————————————————————————————————————————————————————————————————————————————*/
+        public static OnEnterDelegate enter {get; set;}
+        /**———————————————————————————————————————————————————————————————————————————
+          PROP: leave.                                                  <summary>
+          TASK: Method delegate to call after leaving the endpoint.     <br/>
+                Method delegate type must be OnLeaveDelegate.           <br/>
+                The method should be static.                            </summary>
+        ————————————————————————————————————————————————————————————————————————————*/
+        public static OnLeaveDelegate leave {get; set;}
+
+        /**———————————————————————————————————————————————————————————————————————————
+          PROP: requestScopeType.                                       <summary>
+          TASK: This must be a descendant of class RequestScopeBase.    <br/>
+                This allows developers to define a class that gathers   <br/>
+                data tailored for the project from the context.         <br/>
+                Instance will be created by OnActionExecuting,          <br/>
+                Just before ActionShell.enter is called.                <para/>
+                This is an assignment because attribute classes does not<br/>
+                allow generics.                                         </summary>
+        ————————————————————————————————————————————————————————————————————————————*/
+        public static Type requestScopeType {
+            get => _requestScopeType;
+            set {
+                if (value == null)
+                    throw new ArgumentNullException("requestScopeType.");
+                if (!typeof(RequestScopeBase).IsAssignableFrom(value))
+                    throw new ArgumentException("Type Mismatch.");
+                _requestScopeType = value;
+            }
+        }
+
+        /**———————————————————————————————————————————————————————————————————————————
+          FUNC: fetchScope                                              <summary>
+          TASK: Accesses to scope data stored in HttpContext.           <br/>
+          ARGS: httpContext : HttpContext      : Current http context.  <br/>
+          RETV:             : RequestScopeBase : Scope if any or null.  </summary>
+        ————————————————————————————————————————————————————————————————————————————*/
+        public static RequestScopeBase fetchScope(HttpContext httpContext){
+            if (!httpContext.Items.ContainsKey("RequestScope"))
+                return null;
+            return (RequestScopeBase)httpContext.Items["RequestScope"];
+        }
+
+        /**<inheritdoc/>*/
+        public override void OnActionExecuting(ActionExecutingContext context) {
+            RequestScopeBase scope;
+
+            base.OnActionExecuting(context);
+            scope = (RequestScopeBase)
+                Activator.CreateInstance(requestScopeType, new object[]{context});
+            context.HttpContext.Items.Add("RequestScope", scope);
+            enter?.Invoke(scope);
+        } 
+
+        /**<inheritdoc/>*/
+        public override void OnActionExecuted(ActionExecutedContext context) {
+            RequestScopeBase scope = fetchScope(context.HttpContext);
+            leave?.Invoke(scope);
+            if (scope != null)
+                scope.leftAction = true;
+            base.OnActionExecuted(context);
+        }
+
+    }   //  End class ActionShell.
+
+}   //  End namespace.
---
+++ RequestScopeBase.cs
@@ -0,0 +1,44 @@
+﻿/*————————————————————————————————————————————————————————————————————————————
+    ——————————————————————————————————————————————————————
+    |  RequestScopeBase : Base class for request scope.  |
+    ——————————————————————————————————————————————————————
+
+    © Copyright 2020 İhsan Volkan Töre.
+
+Author              : IVT.  (İhsan Volkan Töre)
+Version             : 202201070900
+License             : MIT.
+
+History             :
+202201070900: IVT   : added.
+————————————————————————————————————————————————————————————————————————————*/
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json;
+
+namespace Tore.Service {
+
+    /*————————————————————————————————————————————————————————————————————————————
+        CLASS:  RequestScopeBase
+        USAGE:  Class to contain request data required. 
+                Passed to EnterAction, LeaveAction.
+                Can be used in GlobalExceptionMiddleware.ExceptionResponder too.
+                Extend this according to project requirements.
+    ————————————————————————————————————————————————————————————————————————————*/  
+    public class RequestScopeBase {
+        public string routerPath { get; set; }          // Router path.
+        public string actionName { get; set; }          // Action name.
+        public bool modelValid { get; set; }            // Model state validity.
+        public bool leftAction { get; set; } = false;   // Left action properly.
+        [JsonIgnore]                                    // Maybe logged in json.
+        public ControllerBase controller { get; set; }  // Controller.
+        
+        public RequestScopeBase(ActionExecutingContext context){
+            routerPath = context.HttpContext.Request.Path;
+            actionName = context.ActionDescriptor.RouteValues["Action"];
+            modelValid = context.ModelState.IsValid;
+            controller = (ControllerBase)context.Controller;
+        }
+    }
+}
